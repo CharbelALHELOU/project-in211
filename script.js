aff23ee6590c4fe9094592eb2a6c8aa59e708d8a
Charbel <charbel.alhelou@ensta-paristech.fr>
2020-04-20 07:59:33 +0200

    type write effect and font

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ $(function () {
 function scrollToID( theID ) {
     document.querySelector(theID).scrollIntoView({behavior: 'smooth'});
   }
+
+var i = 0;
+var txt = 'A 4th year student at ENSTA Paris'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+if (i < txt.length) {
+	document.getElementById("msg").innerHTML += txt.charAt(i);
+	i++;
+	setTimeout(typeWriter, speed);
+}
+}
+
+typeWriter();
