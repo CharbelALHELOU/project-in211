21f1c6af399e29ee2e62ac935a0e6354b3a60a66
Charbel <charbel.alhelou@ensta-paristech.fr>
2020-04-20 10:14:33 +0200

    header done

--- script.js
+++ script.js
@@ -19,16 +19,12 @@ function scrollToID( theID ) {
     document.querySelector(theID).scrollIntoView({behavior: 'smooth'});
   }
 
-var i = 0;
-var txt = 'A 4th year student at ENSTA Paris'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
 
-function typeWriter() {
-if (i < txt.length) {
-	document.getElementById("msg").innerHTML += txt.charAt(i);
-	i++;
-	setTimeout(typeWriter, speed);
-}
-}
+  const typewriter = new Typewriter('#msg', {
+	loop: false, delay: 60,
+  });
 
-typeWriter();
+  typewriter.changeCursor(' ').typeString('Hello!').deleteAll().typeString('Charbel Alhelou').start();
+
+  const t2 = new Typewriter('#msg2', {loop:false,delay:60})
+  t2.changeCursor(' ').pauseFor(2000).typeString('A 4th year student at ENSTA Paris').start();
